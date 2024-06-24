f09878f6667597dbb760b87cf39526f5083ae481
Akshat Yadav <71210501+Akshat1276@users.noreply.github.com>
2024-06-24 18:08:58 +0530

    Create decrement.cpp

---
+++ decrement.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x, y;
+    int sum = x+y;
+    int difference = x-y;
+    int product = x*y;
+    int quotient = x/y;
+    int modulus = x%y;
+    int increment = x++;
+    int decrement = x--;
+    return 0;
+}
