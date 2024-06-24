5c6c918931ac218dcf8bafa1c4f3322b8de63bfd
Akshat Yadav <71210501+Akshat1276@users.noreply.github.com>
2024-06-24 18:24:21 +0530

    Create append.cpp

---
+++ append.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string fname = "john ";
+    string lname = "chick";
+    string name = fname.append(lname);
+    cout << name;
+    return 0;
+}
