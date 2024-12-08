5d64aec3c69f6c366be2a352c07f38e57ac57d22
Akshat1276 <akshatyadav1207@gmail.com>
2024-12-08 14:34:34 +0530

    a

--- pointers.cpp
+++ pointers.cpp
@@ -3,10 +3,10 @@
 using namespace std;
 
 int main(){
-    // int initial = 12;
-    // int* y = &initial;
-    // cout << initial << endl;
-    // cout << y;
+    int initial = 12;
+    int* y = &initial;
+    cout << initial << endl;
+    cout << y;
     string food = "f00d1e5";
     string* shet = &food;
     cout << shet << endl;
