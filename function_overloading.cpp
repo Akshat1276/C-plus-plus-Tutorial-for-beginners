0a5222ce8e820eaa7472b33a5a7c1c11f3741d35
Akshat1276 <akshatyadav1207@gmail.com>
2024-12-09 22:25:11 +0530

    function_overloading.cpp

---
+++ function_overloading.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int myFunc(int a){
+    cout << a << endl;
+}
+double myFunc(double a){
+    cout << a << endl;
+}
+bool myFunc(bool a){
+    cout << a << endl;
+}
+
+int main(){
+    myFunc(10);
+    myFunc(3.1);
+    myFunc(true);
+    return 0;
+    // Expected Output: 10, 3.14, 1
+}
