70dfe617a79b5a9f41270ff0a58910021e861891
Akshat1276 <akshatyadav1207@gmail.com>
2024-12-15 17:17:03 +0530

    LL_Traversal

---
+++ Traverse_thru_LL.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+
+    public:
+    Node(int data1, Node* next1){
+        data = data1;
+        next = next1;
+    }
+    Node(int data1){
+        data = data1;
+        next = nullptr;
+    }
+};
+
+Node* CreateLL(vector<int> &arr){
+    Node* head = new Node(arr[0]);
+    Node* mover = head;
+    for(int i=1; i<arr.size(); i++){
+        Node* temp = new Node(arr[i]);
+        mover->next = temp;
+        mover=temp;
+    }
+    return head;
+}
+
+int main(){
+    vector<int> newEG = {12, 17, 18, 4};
+    Node* head = CreateLL(newEG);
+    Node* mover = head;
+    while(mover!=nullptr){
+        cout << mover->data << " ";
+        mover = mover->next;
+    }
+}
