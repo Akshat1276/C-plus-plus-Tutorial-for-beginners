8bc464a3a405027ac0695e4f733fa2e34bf2d0e0
Akshat1276 <akshatyadav1207@gmail.com>
2024-12-15 22:34:49 +0530

    node_addition

---
+++ node_addition_endOf_LL.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <vector>
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node* next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+void printList(Node* node) {
+    while (node != NULL) {
+        cout << node->data << " ";
+        node = node->next;
+    }
+    cout << "\n";
+}
+class Solution {
+  public:
+    Node* insertAtEnd(Node *head, int x) {
+        Node* M1=head;
+        while(M1->next!=nullptr){
+            M1=M1->next;
+        }
+        Node* M2=new Node(x);
+        M1->next=M2;
+        return head;
+    }
+};
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+
+        // Read numbers from the input line
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        int x;
+        cin >> x;
+        cin.ignore();
+
+        Node* head = nullptr;
+
+        // Check if the array is empty
+        if (!arr.empty()) {
+            head = new Node(arr[0]);
+            Node* tail = head;
+            for (size_t i = 1; i < arr.size(); ++i) {
+                tail->next = new Node(arr[i]);
+                tail = tail->next;
+            }
+        }
+
+        Solution ob;
+        Node* ans = ob.insertAtEnd(head, x);
+        printList(ans);
+        cout << "~\n";
+        cout << head->data;
+    }
+    return 0;
+}
