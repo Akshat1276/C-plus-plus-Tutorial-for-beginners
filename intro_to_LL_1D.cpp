93ab0bf3ab823cb8c196380bec922f7021288d73
Akshat1276 <akshatyadav1207@gmail.com>
2024-12-15 16:58:42 +0530

    Create_LL

--- intro_to_LL_1D.cpp
+++ intro_to_LL_1D.cpp
@@ -13,9 +13,28 @@ class Node{
         data = data1;
         next = nullptr;
     }
+
+    public:
+    Node(int data1){
+        data = data1;
+        next = nullptr;
+    }
 };
+
+Node* Convert2LL(vector<int> &arr){
+    Node* head= new Node(arr[0]);
+    Node* mover = head;
+    for(int i=1; i<arr.size(); i++){
+        Node* temp = new Node(arr[i]);
+        mover->next=temp;
+        mover=temp;
+    }
+    return head;
+}
+
 int main(){
-    vector<int> numbers={1,2,3,4,5};
-    Node* y = new Node(numbers[0], nullptr);
-    cout << y->next;
+    vector<int> numbers={12, 4, 3, 54};
+    Node* head = Convert2LL(numbers);
+    cout << head->data;
+
 }
